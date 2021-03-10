c54dbca939702ba7c7122263a284aebcb4002c0d
soualexandre <alexandreifto2@gmail.com>
2021-03-10 16:26:10 -0300

    data de ontem

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -49,7 +49,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
+                'email' => __('Email ou senha incorretos'),
             ]);
         }
 
